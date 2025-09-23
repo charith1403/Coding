984d0a9eb3db760942e62853d02a28ac7e64266d
Charith <65274948+charith1403@users.noreply.github.com>
2025-09-22 20:53:41 -0400

    Time: 0 ms (100.00%) | Memory: 56.2 MB (57.79%) - LeetSync

---
+++ 48-rotate-image/rotate-image.ts
@@ -0,0 +1,17 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function rotate(matrix: number[][]): void {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j <= i; j++) {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++)
+    {
+        matrix[i].reverse();
+    }
+};
