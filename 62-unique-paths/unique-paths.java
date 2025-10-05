0448d1aa48660eeb7e9420026676fb6a0a618ed8
Charith <65274948+charith1403@users.noreply.github.com>
2025-10-05 10:21:02 -0400

    Time: 0 ms (100.00%) | Memory: 40.9 MB (10.64%) - LeetSync

---
+++ 62-unique-paths/unique-paths.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+class Solution {
+    int ans = 0;
+
+    public void uniquePath(int i,int j, int m,int n, int dp[][])
+    {
+        if(i > m || j > n)
+        {
+            return;
+        }
+
+        if(i==m && j==n)
+        {
+            ans++;
+            return;
+        }
+
+        if(dp[i][j] != -1)
+        {
+            ans += dp[i][j];
+            return;
+        }
+
+        int dx[] = {1,0};
+        int dy[] = {0,1};
+        int diffAns = 0;
+
+        for(int k=0;k<2;k++)
+        {
+            int nrow = i + dx[k];
+            int ncol = j + dy[k];
+            int prevAns = ans;
+            uniquePath(nrow,ncol,m,n,dp);
+            diffAns += ans - prevAns;
+        }
+
+        dp[i][j] = diffAns;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int dp[][] = new int [m+1][n+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            Arrays.fill(dp[i],-1);
+        }
+        
+        uniquePath(0,0,m-1,n-1,dp);
+        return ans;
+    }
+}
